f3f2f3d2259b655345437277bfe3a2ab85b3d502
Amir <140071494+Crimson-Amir@users.noreply.github.com>
2024-01-24 16:33:43 +0330

    Delete v2ray-bot/private.py

--- v2ray-bot/private.py
+++
@@ -1,8 +0,0 @@
-telegram_bot_token = "6575677103:AAESMP7V1EKUxiqtZuWKs34SvI-d9CJA9XU"
-auth = {'username': 'amir', 'password': 'amir'}
-PORT = 2053
-ADMIN_CHAT_ID = 6450325872
-DOMAIN = "admin.ggkala.shop"
-protocol = 'https://'
-PRICE_PER_GB = 1_000
-PRICE_PER_DAY = 1_000
