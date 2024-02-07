c449279b81d9458519bda6ca22691522f81110cd
Amir <ytchanneltinos@gmail.com>
2024-02-07 21:59:16 +0330

    pdate_v2ray

--- v2ray-bot/cloudFlear_api.py
+++ v2ray-bot/cloudFlear_api.py
@@ -17,22 +17,20 @@ class CloudFlearApi(ManageDb):
 
         self.connect = requests.Session()
 
+    def make_request(self, method, url, json_data=None):
+        response = self.connect.request(method, url, headers=self.headers, json=json_data, timeout=5)
+        response_json = response.json()
 
-    def make_request(self, method, url, json_data):
-        response = self.connect.request(method, url, json=json_data, timeout=5)
-        try:
-            response.json()
-            print('connect cloouflear Successful!')
-            return True
-        except Exception as e:
-            text = f'🔴 connection problem\ncode: {response.status_code}\nurl: {response.url}'
-            print(text, e)
-            requests.post(
-                telegram_bot_url,
-                data={'chat_id': ADMIN_CHAT_ID, "text": text})
+        if not response_json['success']:
+            text = f'🔴 connection problem in cloudflear\ncode: {response.status_code}\nurl: {response.url}'
+            requests.post(telegram_bot_url, data={'chat_id': ADMIN_CHAT_ID, "text": text})
+            print(f'Connection to CloudFlear Failed. json:\n{response_json}')
             return False
 
-    def change_dns_record_ip(self, record_ip, record_name, proxied, record_type, comment, ttl='Auto', zone_id=None):
+        print('connect cloouflear Successful!')
+        return response_json
+
+    def add_dns_record(self, record_ip, record_name, proxied, record_type, comment, ttl=1, zone_id=None):
         zone_id = zone_id or self.zone_id
 
         data = {
@@ -44,7 +42,29 @@ class CloudFlearApi(ManageDb):
             "ttl": ttl
         }
 
-        req = requests.post(f'https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records',
-                            headers=self.headers, json=data).json()
+        return self.make_request('post', f'https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records', json_data=data)
+
+    def get_dns_record_list(self, zone_id=None):
+        zone_id = zone_id or self.zone_id
+
+        return self.make_request('get', f'https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records')
+
+    def change_dns_record_stting(self, domain_name=None, record_ip=None, record_name=None, proxied=None, record_type=None, comment=None, ttl=1, zone_id=None):
+        zone_id = zone_id or self.zone_id
+
+        get_dns_id = self.get_dns_record_list(zone_id)
+        dns_record_id = [dns_id for dns_id in get_dns_id['result'] if dns_id['name'] == domain_name][0]
+
+        data = {
+            "content": record_ip,
+            "name": record_name,
+            "proxied": proxied,
+            "type": record_type,
+            "comment": comment,
+            "ttl": ttl
+        }
+
+        return self.make_request('post', f'https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records', json_data=data)
+
 
 
