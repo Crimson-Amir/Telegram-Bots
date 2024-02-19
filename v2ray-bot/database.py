3911693b38e9ac73125d90d4aa48d616f26b4c36
Amir <Amir@gmail.com>
2024-02-19 19:27:48 +0330

    update_database

--- v2ray-bot/database.py
+++ v2ray-bot/database.py
@@ -35,6 +35,7 @@ def create_database():
 
     # c.execute('ALTER TABLE Purchased ADD COLUMN auto_renewal integer DEFAULT 0')
 
+
     c.execute('CREATE TABLE IF NOT EXISTS Ticket(id integer primary key, answered integer, chat_id text, last_traffic_usage integer)')
 
     c.execute('CREATE TABLE IF NOT EXISTS Initialization(id integer primary key, answered integer, chat_id text, last_traffic_usage integer)')
