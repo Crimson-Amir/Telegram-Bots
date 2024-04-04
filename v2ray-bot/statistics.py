b724c4edf571bf22f361e04a62c194c572e692a1
Amir <Amir@gmail.com>
2024-04-04 19:02:14 +0330

    update-something

--- v2ray-bot/statistics.py
+++ v2ray-bot/statistics.py
@@ -1,6 +1,8 @@
+import datetime
 from utilities import (ready_report_problem_to_admin)
-from private import *
+import pytz
 from admin_task import (api_operation, sqlite_manager)
+from private import *
 
 
 def pay_per_use_calculator(context):
@@ -13,37 +15,43 @@ def pay_per_use_calculator(context):
     get_last_traffic_uasge = sqlite_manager.select(
         column='last_usage,date',
         table='Last_usage',
-        order_by='id DECS',
+        order_by='id DESC',
         limit=1
     )
 
-    last_usage_dict = {}
+    last_usage_dict = statistics_usage_traffic = {}
 
     try:
         for server in get_all:
             for config in server['obj']:
                 for client in config['clientStats']:
                     for purchased in get_from_db:
-                        if purchased[2] == client['email']:
-                            purchased_wallet = []
-                            last_traffic_usage = [last_traffic_use for last_traffic_use in get_last_traffic_uasge[0][0] if
-                                                  last_traffic_use == purchased[0]]
-                            print(last_traffic_usage)
-                            # if client['enable']:
-                            #
-                            #     upload_mb = client['up'] / (1024 ** 4)
-                            #     download_mb = client['down'] / (1024 ** 4)
-                            #     usage_traffic = upload_mb + download_mb
-                            #
-                            #     traffic_use = (usage_traffic - last_traffic_usage[0][2]) * PRICE_PER_GB
+                        if purchased[2] == client['email'] and client['enable']:
 
-                                # last_usage_dict[]
+                            last_traffic_usage = eval(get_last_traffic_uasge[0][0]).get(str(purchased[0]), 0)
+
+                            upload_mb = client['up'] / (1024 ** 2)
+                            download_mb = client['down'] / (1024 ** 2)
+
+                            usage_traffic = int((upload_mb + download_mb))
+                            traffic_use = int((usage_traffic - last_traffic_usage))
+
+                            last_usage_dict[str(purchased[0])] = usage_traffic
+                            statistics_usage_traffic[str(purchased[0])] = traffic_use
+
+        sqlite_manager.custom(f'INSERT INTO Last_usage (last_usage,date) VALUES ("{str(last_usage_dict)}", "{datetime.datetime.now(pytz.timezone('Asia/Tehran'))}")')
+
+        sqlite_manager.custom(f'INSERT INTO Statistics (traffic_usage,date) VALUES ("{str(statistics_usage_traffic)}", "{datetime.datetime.now(pytz.timezone('Asia/Tehran'))}")')
 
 
 
-                            # else:
-                            #     credit_now = purchased_wallet[0][1]
-
+    except IndexError as e:
+        # ready_report_problem_to_admin(context, 'Statistics Timer IndexError!', '', e)
+        sqlite_manager.insert(table='Last_usage', rows=[{'last_usage': "{}",
+                                                         'date': datetime.datetime.now(pytz.timezone('Asia/Tehran'))}])
 
     except Exception as e:
-        ready_report_problem_to_admin(context, 'PAY PER USE CALCULATOR', '', e)
+        ready_report_problem_to_admin(context, 'Statistics Timer IndexError!', '', e)
+
+
+pay_per_use_calculator(1)
