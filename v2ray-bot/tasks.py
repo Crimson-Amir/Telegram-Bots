fec284dc613237022769a0192f071bdca0d6c54e
Amir <ytchanneltinos@gmail.com>
2024-06-06 23:46:48 +0330

    'update-some-code'

--- v2ray-bot/tasks.py
+++ v2ray-bot/tasks.py
@@ -235,7 +235,7 @@ def payment_page(update, context):
     price = ranking_manage.discount_calculation(query.from_user['id'], direct_price=package[0][7], more_detail=True)
 
     if package[0][7]:
-        keyboard = [[InlineKeyboardButton("درگاه cryptomus", callback_data=f"cryptomus_page_{id_}")],
+        keyboard = [[InlineKeyboardButton("درگاه پرداخت کریپتو", callback_data=f"cryptomus_page_{id_}")],
                     [InlineKeyboardButton("پرداخت از کیف پول", callback_data=f'payment_by_wallet_{id_}'),
                      InlineKeyboardButton("کارت به کارت", callback_data=f'payment_by_card_{id_}')],
                     [InlineKeyboardButton("برگشت ↰", callback_data=f"{package[0][4]}")]]
@@ -930,7 +930,7 @@ def payment_page_upgrade(update, context):
     id_ = int(query.data.replace('service_upgrade_', ''))
     package = sqlite_manager.select(table='User', where=f'chat_id = {chat_id}')
     keyboard = [
-        [InlineKeyboardButton("درگاه cryptomus", callback_data=f"cryptomus_page_upgrade_{id_}")],
+        [InlineKeyboardButton("درگاه پرداخت کریپتو", callback_data=f"cryptomus_page_upgrade_{id_}")],
          [InlineKeyboardButton("پرداخت از کیف پول", callback_data=f'payment_by_wallet_upgrade_service_{id_}'),
          InlineKeyboardButton("کارت به کارت", callback_data=f'upg_ser_by_card{id_}')],
         [InlineKeyboardButton("برگشت ↰", callback_data="my_service")]
@@ -962,7 +962,7 @@ def pay_page_get_evidence_for_upgrade(update, context):
     keyboard = [[InlineKeyboardButton("صفحه اصلی ⤶", callback_data="send_main_message")]]
 
     price = ranking_manage.discount_calculation(chat_id, package[0][5], package[0][6], more_detail=True)
-    check_off = f'\n<b>تخفیف: {price[1]} درصد</b>' if price[1] else ''
+    check_off = f'\n*تخفیف: {price[1]} درصد*' if price[1] else ''
 
     text = (f"\n\nمدت اعتبار فاکتور: 10 دقیقه"
             f"\nسرویس: {package[0][6]} روز - {package[0][5]} گیگابایت"
@@ -1668,7 +1668,7 @@ def pay_way_for_credit(update, context):
     package = sqlite_manager.select(column='value', table='Credit_History', where=f'id = {id_}')
 
     keyboard = [
-        [InlineKeyboardButton("درگاه cryptomus", callback_data=f"cryptomus_page_wallet_{id_}")],
+        [InlineKeyboardButton("درگاه پرداخت کریپتو", callback_data=f"cryptomus_page_wallet_{id_}")],
         [InlineKeyboardButton("کارت به کارت", callback_data=f'pay_by_card_for_credit_{id_}')],
         [InlineKeyboardButton("برگشت ↰", callback_data="buy_credit_volume")]
     ]
